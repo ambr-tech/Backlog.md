a3544cf4939d4874a2db4a29d7469b64774c4cdc
amp <hlt315@gmail.com>
2026-06-01 10:58:06 +0900

    Rename

---
+++ scripts/delete-dist-run-backlog.bat
@@ -0,0 +1,4 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0delete-dist-run-backlog.ps1"
+
+pause
--- scripts/delete_dist_run_backlog.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0delete_dist_run_backlog.ps1"
-
-pause
